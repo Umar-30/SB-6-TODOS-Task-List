6817ee6f30f0f1fdd445dc2d2329a6ec3acf56dd
Shagufta Zakir <159715160+Umar-30@users.noreply.github.com>
2024-05-31 08:43:22 -0700

    This is colorful TODOS Task list

---
+++ index.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let todoList = [];
+let condition = true;
+
+console.log(chalk.red("\n \t Wellcome to SB-6 TODO-List Appliciation\n"));
+
+while(condition){
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: chalk.yellow("Enter Your New Task :")
+        }
+    ]);
+    todoList.push(addTask.task);
+    console.log(`${addTask.task} Task added in Todo-list successfully`);
+
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: chalk.blue("Do you want to more task ?"),
+            default: "False"
+        }
+    ]);
+    condition = addMoreTask.addmore
+}
+console.log("your updated Todo-list:" ,todoList);
